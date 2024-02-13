906a5bf38802f309e8af21b735f0a00fd9c4e556
Frenzoid <frenzoid@protonmail.com>
2024-02-13 21:22:48 +0100

    updated apirul

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   // output: 'export',
   reactStrictMode: true,
   publicRuntimeConfig: {
-    APIURL: process.env.APIURL ?? 'http://localhost:3001/api',
+    APIURL: process.env.APIURL ?? 'https://semiunarium.frenzoid.dev/api',
   },
 };
 
